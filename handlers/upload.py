22e11669af91e93522b2ddaafa9888e76d9688de
peter.jirak <peter.jirak@gmail.com>
2016-04-22 10:33:23 -0500

    Cleaned up templates/art.html and templates/index.html
    Modified post in handlers/upload.py to respond with JSON object to cause
    upload jQueryUI eidget to work correctly.

--- handlers/upload.py
+++ handlers/upload.py
@@ -1,3 +1,5 @@
+import json
+
 from google.appengine.api import users
 from google.appengine.api import images
 
@@ -8,20 +10,33 @@ from models.art import Art
 
 class UploadHandler(BasePageHandler):
     def get(self):
-        # TODO : Require login to render the upload template:
         if not users.get_current_user():
             return self.redirect("/register_user")
         template = self.get_template('templates/file_upload.html')
         self.response.write(template.render(self.template_values))
 
     def post(self):
-        # TODO : Handle post request from non-logged in user (Respond with an HTTP Error code of 403 -- forbidden
+        # TODO : Handle post request from non-logged in user (Respond with an HTTP Error code of 403 -- forbidden)
+        # TODO : Make adding an image to the Google datastore strongly consistent
+        # TODO : Create a URL for the image detail page -- add that URL to the response in the url field
+        # TODO : Add support for image thumbnails.
         new_art = Art()
         if users.get_current_user():
             new_art.user_id = users.get_current_user().user_id()
         new_art.title = self.request.get('title[]')
         new_art.description = self.request.get('description[]')
+        filename = self.request.POST.get('userfile').filename
         art_image = self.request.get('userfile')
+        file_size = len(art_image)
         art_image = images.resize(art_image, 600, 600)
         new_art.image = art_image
-        new_art.put()
+
+        response_body = {
+            "files": [ {
+                "name": filename,
+                "size": file_size
+            } ]
+        }
+
+        self.response.headers['Content-Type'] = 'image/jpeg'
+        self.response.write(json.dumps(response_body))
