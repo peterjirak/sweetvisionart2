a3fb3af0d17cfda92bb383649cd163c754b2878e
peter.jirak <peter.jirak@gmail.com>
2016-04-24 20:25:53 -0500

    Removed completed TODO from handlers/upload.py

--- handlers/upload.py
+++ handlers/upload.py
@@ -20,7 +20,6 @@ class UploadHandler(BasePageHandler):
         # TODO : Handle post request from non-logged in user (Respond with an HTTP Error code of 403 -- forbidden)
         # TODO : Make adding an image to the Google datastore strongly consistent
         # TODO : Create a URL for the image detail page -- add that URL to the response in the url field
-        # TODO : Add support for image thumbnails.
         new_art = Art()
         if users.get_current_user():
             new_art.user_id = users.get_current_user().user_id()
