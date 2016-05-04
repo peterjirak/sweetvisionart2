26dabbeb52527aec9d1d73bdcc61134642a1d0f5
peter.jirak <peter.jirak@gmail.com>
2016-05-03 21:14:30 -0500

    Modified: handlers/register_user.py           templates/register_user.html
    Modified the register_user functionality to support changing a user's
    first, last, and middle names.

--- handlers/register_user.py
+++ handlers/register_user.py
@@ -1,4 +1,5 @@
 import re
+import urllib
 
 from google.appengine.api import users
 
@@ -21,19 +22,43 @@ class RegisterUserHandler(BaseAuthenticatedUserPageHandler):
             raise UnauthorizedException("/register_user invoked but user has not been authenticated. " +
                                         "Unable to handle request.")
 
+        feedback = self.request.get('feedback')
+        feedback_status = self.request.get('feedback_status')
+
+        if feedback is not None and not re.match(r"^\s*$", feedback):
+            feedback = feedback.strip()
+            self.template_values['feedback'] = feedback
+            if feedback_status is None or re.match(r"^\s*$", feedback_status):
+                feedback_status = 'No_Change'
+            feedback_status = feedback_status.strip()
+            self.template_values['feedback_status'] = feedback_status
+
         continue_to = self.request.get('continue')
         if continue_to is None or re.match(r"^\s*$", continue_to):
             continue_to = '/'
 
+        self.template_values['is_update'] = False
         if self.application_user:
-            # The current user is already registered -- no need to display the registration form
-            return self.redirect(continue_to)
+            # The user has already registered -- load the template values with the existing
+            # values for the user to permit the user to change his or her registration information:
+            self.template_values['is_update'] = True
+            if self.request.path is not None and not re.match(r"r^\s*$", self.request.path):
+                continue_to = self.request.path
+            else:
+                continue_to = '/register_user'
+            if self.application_user.first_name is not None and \
+               not re.match(r"^\s*$", self.application_user.first_name):
+                self.template_values['first_name'] = self.application_user.first_name
+
+            if self.application_user.first_name is not None and \
+               not re.match(r"^\s*$", self.application_user.first_name):
+                self.template_values['middle_name'] = self.application_user.middle_name
+
+            if self.application_user.last_name is not None and \
+               not re.match(r"^\s*$", self.application_user.last_name):
+                self.template_values['last_name'] = self.application_user.last_name
 
         self.template_values['after_registration_continue_to'] = continue_to
-
-        if self.application_user is not None:
-            # User is logged in and already registered, do not re-register:
-            return self.redirect('/')
 
         # Get email from Google Auth user object if available:
         if self.google_user.email():
@@ -57,16 +82,14 @@ class RegisterUserHandler(BaseAuthenticatedUserPageHandler):
             continue_to = '/'
         continue_to = str(continue_to)
 
-        if not self.logged_in:
+        google_user = self.google_user
+        if not self.logged_in or not google_user:
             # Must login using Google Auth before registering:
             self.redirect(users.create_login_url(continue_to))
-        elif self.application_user is not None:
-            # User is logged in and already registered, do not re-register:
-            self.redirect(continue_to)
+            return # return after redirect
 
-        google_user_id = self.google_user.user_id()
-        email = self.google_user.email()
-
+        google_user_id = google_user.user_id()
+        email = google_user.email()
         first_name = self.request.get('first_name')
         middle_name = self.request.get('middle_name')
         last_name = self.request.get('last_name')
@@ -74,7 +97,81 @@ class RegisterUserHandler(BaseAuthenticatedUserPageHandler):
         if email is None or re.match(r"^\s*$", email):
             email = self.request.get('email')
 
-        User.add_or_get_user(google_user_id=google_user_id, email=email, first_name=first_name,
-                             middle_name=middle_name, last_name=last_name)
+        if first_name is not None:
+            first_name = first_name.strip()
 
-        return self.redirect(continue_to)
+        if last_name is not None:
+            last_name = last_name.strip()
+
+        if middle_name is not None:
+            middle_name = middle_name.strip()
+
+        if email is not None:
+            email = email.strip()
+
+        application_user = self.application_user
+
+        if application_user is not None:
+            # POST request is to update the registration of an existing user:
+            has_changed = False
+            if first_name is not None and not re.match(r"^\s*$", first_name):
+                if first_name != application_user.first_name:
+                    application_user.first_name = first_name
+                    has_changed = True
+
+            if last_name is not None and not re.match(r"^\s*$", last_name):
+                if last_name != application_user.last_name:
+                    application_user.last_name = last_name
+                    has_changed = True
+
+            if middle_name is None or middle_name == '':
+                if application_user.middle_name is not None and not re.match(r"^\s*$", application_user.middle_name):
+                    application_user.middle_name = None
+                    has_changed = True
+            else:
+                if application_user.middle_name is None or re.match(r"^\s*$", application_user.middle_name):
+                    application_user.middle_name = middle_name
+                    has_changed = True
+                elif application_user.middle_name != application_user.middle_name:
+                    application_user.middle_name = middle_name
+                    has_changed = True
+
+            if has_changed:
+                update_success = True
+                try:
+                    application_user.put()
+                except:
+                    update_success = False
+                if update_success:
+                    feedback = 'Successfully updated your registration.'
+                    feedback_status = 'Success'
+                else:
+                    feedback = 'Attempt to update your registration failed.'
+                    feedback_status = 'Failure'
+            else:
+                feedback = 'Your registration information is unchanged.'
+                feedback_status = 'No_Change'
+        else:
+            # POST request is to register the user:
+            create_success = True
+            try:
+                application_user = User.add_or_get_user(google_user_id=google_user_id, email=email,
+                                                        first_name=first_name, middle_name=middle_name,
+                                                        last_name=last_name)
+            except:
+                create_success = False
+
+            if create_success and application_user is None:
+                create_success = False
+
+            if create_success:
+                feedback = 'You have been registered.'
+                feedback_status = 'Success'
+            else:
+                feedback = 'Registration failed.'
+                feedback_status = 'Failure'
+
+        feedback = urllib.quote(feedback, safe='')
+        feedback_status = urllib.quote(feedback_status, safe='')
+
+        return self.redirect(continue_to + '?feedback=' + feedback + '&feedback_status=' + feedback_status)
